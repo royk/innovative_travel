9a694a5c95858ff88d5209c3e0766cd5b71e01a8
Roy Klein <roeiklein@gmail.com>
2015-10-12 19:30:05 +0300

    Switched to mock server for data.

--- app/models/hotel.js
+++ app/models/hotel.js
@@ -1,43 +1,8 @@
 import DS from 'ember-data';
 
-let Hotel = DS.Model.extend({
+export default DS.Model.extend({
   name: DS.attr(),
   hotelCount: DS.attr(),
   type: DS.attr(),
   subtype: DS.attr()
 });
-
-Hotel.reopenClass({
-  FIXTURES: [
-    {
-      "name": "Hotel Espresso Amsterdam",
-      "id": "hotel:Hotel_Espresso_Ams",
-      "hotelCount": "1",
-      "type": "hotel",
-      "subtype": "hotel"
-    },
-    {
-      "name": "Hotel Buena Vista",
-      "id": "hotel:Hotel_Buena_Vista",
-      "hotelCount": "1",
-      "type": "hotel",
-      "subtype": "hotel"
-    },
-    {
-      "name": "Amsterdam",
-      "id": "place:Amsterdam",
-      "hotelCount": "329",
-      "type": "place",
-      "subtype": "city"
-    },
-    {
-      "name": "Amsterdam Schiphol Airport (AMS)",
-      "id": "place:Amsterdam_Schiphol",
-      "hotelCount": "15",
-      "type": "place",
-      "subtype": "airport"
-    }
-  ]
-});
-
-export default Hotel
